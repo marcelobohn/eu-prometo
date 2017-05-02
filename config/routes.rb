8142ceb2824bad13855e38175f772f785da95b9a
Marcelo <marcelo.bohn@consyste.com.br>
2017-05-02 10:32:23 -0300

    created controller contact

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   get 'about', to: 'about#index'
+  get 'contact', to: 'contact#index'
 
   resources :elections
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
