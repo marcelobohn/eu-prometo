77889ca365ad42cd185251853439534e47c462b9
marcelo.bohn <marcelo.bohn@gmail.com>
2018-02-09 11:04:57 -0500

    review address tests

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -3,7 +3,7 @@ class AddressController < ApplicationController
     raise "Argumentos faltando na requisição" if !params[:id]
     @cities = City.order(:name).where(state_id: params[:id]).select(:id, :name)
     if (params[:needs_manager])
-      @cities = @cities.joins('FULL JOIN "managers" ON "managers"."city_id" = "cities"."id"').where('managers.city_id is null')    
+      @cities = @cities.where('cities.id not in (select city_id from managers) ')    
     end
     render json: @cities    
   end
--- spec/controllers/address_controller_spec.rb
+++ spec/controllers/address_controller_spec.rb
@@ -1,15 +1,39 @@
 require 'rails_helper'
 
-# require 'spec_helper'
-
 RSpec.describe AddressController, type: :controller do
+  let(:election) {create(:election)}
+  let(:user) {create(:user, :admin)}
+  let(:country) {create(:country)}
+  let(:state) {create(:state, country: country)}
+  
   describe "POST #cities" do
     it "returns http success" do
-      post :cities, { :params => { :id => 23 }, :format => :json }
-      # expect(response).to have_http_status(:success)
+      create(:city, state: state)
+      create(:city, name: 'Novo Hamburgo', state: state)
+
+      post :cities, { :params => { :id => state.id }, :format => :json }
+      json_body = JSON.parse(response.body)
+      
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq('application/json')
+      expect(json_body.size).to eq 2
+    end
+    
+    it "filter cities needs a manager" do
+      ivoti = create(:city, state: state)
+      create(:city, name: 'Novo Hamburgo', state: state)
+      manager = create :manager, election: election,
+        country_id: country.id, state_id: state.id, city_id: ivoti.id,
+        type_manager: :mayor, user: user      
+
+      
+      post :cities, { :params => { :id => state.id, :needs_manager => 'true' }, :format => :json }
+      json_body = JSON.parse(response.body)
+      
       expect(response).to be_success
       expect(response.content_type).to eq('application/json')
-    end
+      expect(json_body.size).to eq 1
+    end    
 
     it "returns http error, needs parameter" do
       expect { post :cities, format: :json }.to raise_error RuntimeError, 'Argumentos faltando na requisição'
